6e0930460b2670b22b2fbfa6a9ca3804596a5ca0
andy <andrew.stillie@gmail.com>
2014-07-31 23:01:23 +0100

    Initial Commit

---
+++ Test++/src/Test.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+	for (i = 0 ; i<10;i++)
+	cout << "!!!Hello BeagleBone !! 1st May 2014" << endl; // prints !!!Hello BeagleBone!!
+	return 0;
+}
